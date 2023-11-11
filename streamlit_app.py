91d795e81ca8e604d5017ecc7aab8f2fecba547f
snowflaketestlavanya <150498279+snowflaketestlavanya@users.noreply.github.com>
2023-11-10 22:43:38 -0600

    Update streamlit_app.py
    updated with 'Favorites' and 'Avocado Toast'

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents new healthy diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry oatmeal')
-streamlit.text('Kale,Spinach & Rocket Smoothie')
-streamlit.text('Hard-boiled free range egg')
+streamlit.header('Breakfast Favorites')
+   
+streamlit.text('🥣 Omega 3 & Blueberry oatmeal')
+streamlit.text('🥗 Kale,Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-boiled free range egg')
+streamlit.text('🥑🍞 Avocado Toast')
