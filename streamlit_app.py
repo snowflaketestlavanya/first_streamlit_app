ba7cc8515bb4cf3789a2434cec0b74b130a9c00b
snowflaketestlavanya <150498279+snowflaketestlavanya@users.noreply.github.com>
2023-11-16 20:31:33 -0600

    Update streamlit_app.py
    json and table format

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,3 +26,10 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# write your own comment -what does the next line do? 
+#json format
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+# table format
+streamlit.dataframe(fruityvice_normalized)
