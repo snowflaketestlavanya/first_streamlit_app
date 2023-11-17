f6c08cf2c10bab58229234734d9955fa75630f47
snowflaketestlavanya <150498279+snowflaketestlavanya@users.noreply.github.com>
2023-11-16 22:12:12 -0600

    Update streamlit_app.py
    snowflake.connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,9 +29,9 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 #streamlit.text(fruityvice_response.json())
 
-# write your own comment -what does the next line do? 
-#json format
+# write your own comment -what does the next line do? #json format
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-# table format
+# write your own comment - what does this do?# table format
 streamlit.dataframe(fruityvice_normalized)
+
+import snowflake.connector
